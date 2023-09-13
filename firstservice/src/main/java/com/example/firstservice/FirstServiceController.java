2b7c08f771df6a576f87c73f3d27134e8e270714
a60156077 <dwrhksfl77@naver.com>
2023-09-13 16:56:19 +0900

    eureka client 적용

--- firstservice/src/main/java/com/example/firstservice/FirstServiceController.java
+++ firstservice/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/first-service")
+@RequestMapping("/")
 public class FirstServiceController {
 
     @GetMapping("/welcome")
