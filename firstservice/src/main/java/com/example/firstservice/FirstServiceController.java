b40ca6230d3012250e675b4af645f00102d10a0d
a60156077 <dwrhksfl77@naver.com>
2023-09-13 17:04:53 +0900

    re import first service

---
+++ firstservice/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,29 @@
+package com.example.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+public class FirstServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome service1";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info("header {}", header);
+        return "hello message";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "hello this is check from first service";
+    }
+}
