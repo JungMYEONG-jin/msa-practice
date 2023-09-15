fe790e29035feca56b7d455fca8c83f4e345a084
a60156077 <dwrhksfl77@naver.com>
2023-09-15 13:53:39 +0900

    first service 랜덤포트로 변경.

--- firstservice/src/main/java/com/example/firstservice/FirstServiceController.java
+++ firstservice/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class FirstServiceController {
 
     @GetMapping("/welcome")
