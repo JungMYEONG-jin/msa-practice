68b2af0d7d2d5542e8a7a7fa591f4f3aee5eea8f
a60156077 <dwrhksfl77@naver.com>
2023-09-15 15:25:26 +0900

    micro user service 1. add greeting method

--- microuserservice/src/main/java/com/example/microuserservice/adapter/in/controller/UserController.java
+++ microuserservice/src/main/java/com/example/microuserservice/adapter/in/controller/UserController.java
@@ -1,24 +1,28 @@
 package com.example.microuserservice.adapter.in.controller;
 
+import com.example.microuserservice.prop.Greeting;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
+@RequiredArgsConstructor
 public class UserController {
-
+    private final Greeting greeting;
     private final Environment environment;
-
-    public UserController(Environment environment) {
-        this.environment = environment;
-    }
 
     @GetMapping("/health-check")
     public String healthCheck(HttpServletRequest request) {
         return String.format("Working my port is %s", request.getServerPort());
+    }
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return String.format("Greeting my message is %s", greeting.getMessage());
     }
 
     @GetMapping("/welcome")
