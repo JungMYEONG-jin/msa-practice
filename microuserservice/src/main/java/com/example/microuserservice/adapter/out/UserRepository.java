703ab5395460155a8182dbce6d4fe66fae0e144f
a60156077 <dwrhksfl77@naver.com>
2023-09-15 15:45:08 +0900

    micro user service 1. repository extends

--- microuserservice/src/main/java/com/example/microuserservice/adapter/out/UserRepository.java
+++ microuserservice/src/main/java/com/example/microuserservice/adapter/out/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.microuserservice.adapter.out;
 import com.example.microuserservice.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, UserEntity> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
