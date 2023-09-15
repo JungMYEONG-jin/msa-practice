4884580da2032984d51ed9b9a5a55b48da6af2b0
godcoder <dwrhksfl77@naver.com>
2023-09-16 08:51:49 +0900

    spring SecurityConfig 작성 완료. 3.x.x 버전에서는 어떤 request servlet을 사용할지 명시해줘야 오류가 발생 안함.

--- microuserservice/src/main/java/com/example/microuserservice/config/SecurityConfig.java
+++ microuserservice/src/main/java/com/example/microuserservice/config/SecurityConfig.java
@@ -1,20 +1,54 @@
 package com.example.microuserservice.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().
+                requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                .requestMatchers(new AntPathRequestMatcher( "/favicon.ico"))
+                .requestMatchers(new AntPathRequestMatcher( "/css/**"))
+                .requestMatchers(new AntPathRequestMatcher( "/js/**"))
+                .requestMatchers(new AntPathRequestMatcher( "/img/**"))
+                .requestMatchers(new AntPathRequestMatcher( "/lib/**"));
+    }
+
+    /**
+     * 1/4 현재 개발중이므로 cors 그냥 disable 해놓자
+     * 추후 배포에 cors 적용
+     * requestMatchers(CorsUtils::isPreFlightRequest).permitAll() xhr fail되면 이거 적용
+     * @param http
+     * @return
+     */
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(new MvcRequestMatcher(introspector, "/user-service/**")).permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
 }
