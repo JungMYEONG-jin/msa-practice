f5542858f977a81729bfde8b90a6349dbdc85616
a60156077 <dwrhksfl77@naver.com>
2023-09-15 16:48:40 +0900

    micro user service 1. add validatio on DTO

--- microuserservice/src/main/java/com/example/microuserservice/data/RequestUser.java
+++ microuserservice/src/main/java/com/example/microuserservice/data/RequestUser.java
@@ -1,12 +1,19 @@
 package com.example.microuserservice.data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class RequestUser {
+    @NotBlank(message = "이름은 필수입력값입니다.")
     private String name;
+    @NotBlank(message = "Email은 공백이 불가합니다.")
+    @Email
     private String email;
+    @Size(min = 8, message = "패스워드는 최소 8자 이상입니다.")
     private String pwd;
 }
