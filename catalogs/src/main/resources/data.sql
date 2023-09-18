e1996dd38c46bff1fa4ba1d339b4f3b3be4c3363
a60156077 <dwrhksfl77@naver.com>
2023-09-18 13:36:45 +0900

    MSA Catalog Service 1. Add insert sql

---
+++ catalogs/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into catalog (product_id, product_name, stock, unit_price) values ("CATALOG-0001", "pizza", 100, 15000);
+insert into catalog (product_id, product_name, stock, unit_price) values ("CATALOG-0002", "chicken", 200, 18000);
+insert into catalog (product_id, product_name, stock, unit_price) values ("CATALOG-0003", "noodle", 1000, 8000);
