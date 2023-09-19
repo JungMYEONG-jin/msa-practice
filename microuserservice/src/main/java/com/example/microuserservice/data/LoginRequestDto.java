6c74d93fe297acc40e7dfa0acb88ec2e928405e0
a60156077 <dwrhksfl77@naver.com>
2023-09-19 10:18:00 +0900

    micro user service add login request, response dto

---
+++ microuserservice/src/main/java/com/example/microuserservice/data/LoginRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.microuserservice.data;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginRequestDto {
+    @NotNull(message = "Email can not be null")
+    @Size(min = 2, message = "Please typing more than 2 characters")
+    @Email
+    private String email;
+
+    @NotNull(message = "Password can not be null")
+    @Size(min = 8, message = "Password must be equal or greater than 8 characters")
+    private String password;
+}
---
+++ microuserservice/src/main/java/com/example/microuserservice/data/LoginResponseDto.java
@@ -0,0 +1,6 @@
+package com.example.microuserservice.data;
+
+public class LoginResponseDto {
+    private String token;
+    private String userId;
+}
