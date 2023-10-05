6a08af428773381b456f640f924d2fb2e832fdcf
a60156077 <dwrhksfl77@naver.com>
2023-10-05 14:14:50 +0900

    MSA Catalog Service 1. add spring kafka 2. add consumer config 3. add consumer service

---
+++ catalogs/src/main/java/com/example/catalogs/mq/KafkaConsumer.java
@@ -0,0 +1,47 @@
+package com.example.catalogs.mq;
+
+import com.example.catalogs.adapter.out.persistence.CatalogEntity;
+import com.example.catalogs.adapter.out.persistence.CatalogRepository;
+import com.example.catalogs.domain.Catalog;
+import com.example.catalogs.port.out.CatalogFindPort;
+import com.example.catalogs.port.out.CatalogSavePort;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumer {
+    private final CatalogFindPort catalogFindPort;
+    private final CatalogSavePort catalogSavePort;
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String message) {
+        log.info("message {}", message);
+        Map<Object, Object> map = new HashMap<>();
+        try{
+            map = objectMapper.readValue(message, new TypeReference<Map<Object, Object>>() {
+            });
+        }catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        Catalog catalog = catalogFindPort.findByProductId((String) map.get("productId"));
+        if (catalog != null) {
+            catalog.setStock(catalog.getStock() - (Integer) map.get("qty"));
+            catalogSavePort.save(catalog);
+        }
+    }
+}
