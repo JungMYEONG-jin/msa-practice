d2f762254837617363d3769c7088f0d546c7ee34
a60156077 <dwrhksfl77@naver.com>
2023-10-05 14:52:40 +0900

    MSA Order Service 1. send topic with CompletableFuture

--- order/src/main/java/com/example/order/mq/KafkaProducer.java
+++ order/src/main/java/com/example/order/mq/KafkaProducer.java
@@ -6,24 +6,35 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaProducer {
-    private KafkaTemplate<String, String> kafkaTemplate;
-    private ObjectMapper objectMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
     public OrderDto send(String topic, OrderDto orderDto) {
         String json = "";
+//        ObjectMapper objectMapper = new ObjectMapper();
         try{
             json = objectMapper.writeValueAsString(orderDto);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        kafkaTemplate.send(topic, json);
-        log.info("kafka producer send data from order micro service {}", orderDto);
+
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(topic, json);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("kafka producer send data from order micro service {}, offset {}", orderDto, result.getRecordMetadata().offset());
+            } else {
+                log.info("unable to send message {}", ex.getMessage());
+            }
+        });
         return orderDto;
     }
 }
